10ce1c53251ac7f3e79a3ece005ef3fb903b9644
rulaa <43814466+rulaa@users.noreply.github.com>
2018-11-09 21:28:20 -0500

    Add files via upload

---
+++ Update1-master/FirstTasks/Project 1/js/app.js
@@ -0,0 +1,29 @@
+function dropdown_function() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function updateCurrentDate() {
+  var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = months[today.getMonth()];
+  var yyyy = today.getFullYear();
+
+  document.getElementById('current-date').innerHTML = `${mm} ${dd}, ${yyyy}`;
+}
+updateCurrentDate();
